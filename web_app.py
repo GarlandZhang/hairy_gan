ff2cd2dd008bddc206f49d1143d953a00074c045
itsGarland <garland._zhang@hotmail.com>
2020-07-27 20:44:21 +0000

    only allow post requests

--- web_app.py
+++ web_app.py
@@ -11,11 +11,9 @@ def main():
   prediction_path = './static/prediction.png'
   return render_template('index.html', prediction_path=prediction_path)
 
-@app.route('/predictions', methods=['GET', 'POST']) # GET for user, POST for receiving predictions from model
+@app.route('/predictions', methods=['POST']) # GET for user, POST for receiving predictions from model
 def predictions():
-  if request.method == 'GET':
-    return 'Display predictions'
-  elif request.method == 'POST':
+  if request.method == 'POST':
     img_prop_enc = request.form.get('img_props')
     img_prop = list(base64.b64decode(img_prop_enc))
 
